a7f778183ef64524e2a2fbc0d7c62513c2630703
Shivi Gargi <77526749+shivi194@users.noreply.github.com>
2021-10-12 12:33:46 +0530

    Library Management System
    Library Management System

---
+++ README.md
@@ -0,0 +1,63 @@
+### Front-end
+
+1. HTML5
+2. CSS3
+3. BOOTSTRAP 4
+4. jQuery
+
+### Back-end
+
+1. MongoDB
+2. Express.js
+3. Node.js
+4. Passport.js
+
+## Install dependencies
+
+npm i --save
+
+- App will open at http://localhost:3000
+
+## Functionalitites
+
+App section:
+
+- Admin
+- User
+- Browse books
+
+### Admin module functionalities
+
+- Sign up (This route is hidden. only accessible by typing the route manually and when admin log in)
+- Login
+- Logout
+- Track all users activities
+- Add books
+- Update books
+- Delete books
+- Search books by category, title, author, ISBN
+- Find users by firstname, lastname, email and username
+- Delete user acount
+- Restrict individual user if violate any terms and conditions
+- Browse books
+- Update admin profile and password
+- Add new admin
+- Delete currently logged in admin profile
+
+### User module functionalities
+
+- Sign up
+- Login
+- Logout
+- Track own activities
+- Issue books
+- Renew books
+- Return books
+- Browse books showcase
+- Add, edit and delete comment on any books comment section
+- Update profile and password
+- Delete account
+
+### Browse books module functionalities
+- Show all books
+- Find books via filter
---
+++ controllers/books.js
@@ -0,0 +1,89 @@
+const Book = require('../models/book');
+const PER_PAGE = 16;
+
+
+
+exports.getBooks = async(req, res, next) => {
+    var page = req.params.page || 1;
+    const filter = req.params.filter;
+    const value = req.params.value;
+    let searchObj = {};
+   
+    // constructing search object
+    if(filter != 'all' && value != 'all') {
+       // fetch books by search value and filter
+       searchObj[filter] = value;
+    }
+   console.log('SearchObj :',searchObj)
+    try {
+       // Fetch books from database
+       const books = await Book
+       .find(searchObj)
+       .skip((PER_PAGE * page) - PER_PAGE)
+       .limit(PER_PAGE);
+
+       // Get the count of total available book of given filter
+       const count = await Book.find(searchObj).countDocuments();
+ 
+       res.render("books", {
+          books: books,
+          current: page,
+          pages: Math.ceil(count / PER_PAGE),
+          filter: filter,
+          value: value,
+          user: req.user,
+       })
+    } catch(err) {
+       console.log(err)
+    }
+}
+
+exports.findBooks = async(req, res, next) => {
+   
+   var page = req.params.page || 1;
+   const filter = req.body.filter.toLowerCase();
+   const value = req.body.searchName;
+
+   // show flash message if empty search field is sent to backend
+   if(value == "") {
+       req.flash("error", "Searc field is empty. Please fill the search field in order to get a result");
+       return res.redirect('back');
+   }
+
+   const searchObj = {};
+   searchObj[filter] = value;
+   console.log('FindBooks :',searchObj)
+   try {
+      // Fetch books from database
+      const books = await Book
+      .find(searchObj)
+      .skip((PER_PAGE * page) - PER_PAGE)
+      .limit(PER_PAGE)
+
+      // Get the count of total available book of given filter
+      const count = await Book.find(searchObj).countDocuments();
+      
+      res.render("books", {
+         books: books,
+         current: page,
+         pages: Math.ceil(count / PER_PAGE),
+         filter: filter,
+         value: value,
+         user: req.user,
+      })
+   } catch(err) {
+      console.log(err)
+   }
+}
+
+//Get Book Details by specific book id
+exports.getBookDetails = async(req, res, next) => {
+   try {
+      const book_id = req.params.book_id;
+      const book = await Book.findById(book_id).populate("comments");
+      res.render("user/bookDetails", {book: book});
+   } catch (err) {
+      console.log(err);
+      return res.redirect("back");
+   }
+}
